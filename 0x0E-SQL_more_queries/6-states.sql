744260d6d0073f19f1f2831248a6a6b50d3f6f60
pelzmagic <akinloyepelz@gmail.com>
2022-08-12 05:00:33 -0700

    hot fix

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
----creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server
+--creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server
 -- creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- use a database
