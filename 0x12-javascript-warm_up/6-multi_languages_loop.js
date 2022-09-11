76509f71e7e1c9095c4eae671135882891118471
pelzmagic <akinloyepelz@gmail.com>
2022-09-11 09:37:36 +0000

    print three lines using an array of string and a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// ['C is fun', 'Python is cool', 'Javascript is amazing'].forEach((e) => console.log(e));
+
+const msg = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+msg.forEach((element) => console.log(element));
