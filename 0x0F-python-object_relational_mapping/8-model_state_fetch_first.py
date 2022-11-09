6bb17c706282511f7cb58b5cce0ba9a4ff80667e
Akinloye Oluwapelumi <83060434+pelzmagic@users.noreply.github.com>
2022-11-09 09:23:12 -0800

    Update 8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -10,15 +10,19 @@ from sys import argv
 from model_state import Base, State
 
 if __name__ == "__main__":
-        eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                                                argv[2],
-                                                                                                                                                    argv[3]))
-            Base.metadata.create_all(eng)
-                Session = sessionmaker(bind=eng)
-                    session = Session()
-                        first_state = session.query(State).order_by(State.id).first()
-                            if first_state is not None:
-                                        print("{}: {}".format(first_state.id, first_state.name))
-                                            else:
-                                                        print("Nothing")
-                                                            session.close()
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    first_state = session.query(State).order_by(State.id).first()
+    if first_state is not None:
+        print("{}: {}".format(first_state.id, first_state.name))
+    else:
+        print("Nothing")
+    session.close()
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
