305d798012e7ba5cd5067020aa991a61e43adaa8
Akinloye Oluwapelumi <83060434+pelzmagic@users.noreply.github.com>
2022-11-09 09:27:26 -0800

    Update 12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,4 +1,6 @@
-anges the name of the State object where id=2 to New Mexico from a database
+#!/usr/bin/python3
+"""
+changes the name of the State object where id=2 to New Mexico from a database
 """
 
 import sqlalchemy
@@ -9,13 +11,13 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-        eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                                            argv[2],
-                                                                                                                                                argv[3]))
-                                                                                                                                                    Base.metadata.create_all(eng)
-                                                                                                                                                        Session = sessionmaker(bind=eng)
-                                                                                                                                                            session = Session()
-                                                                                                                                                                state = session.query(State).filter_by(id=2).first()
-                                                                                                                                                                    state.name = "New Mexico"
-                                                                                                                                                                        session.commit()
-                                                                                                                                                                            session.close()01~
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    state = session.query(State).filter_by(id=2).first()
+    state.name = "New Mexico"
+    session.commit()
+    session.close()
