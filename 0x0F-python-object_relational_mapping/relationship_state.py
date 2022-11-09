0a91b42e5af792b94a9949c375e561ebd437ba28
Akinloye Oluwapelumi <83060434+pelzmagic@users.noreply.github.com>
2022-11-09 10:39:39 -0800

    Update relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -12,8 +12,8 @@ Base = declarative_base()
 
 
 class State(Base):
-        """Representation of a state"""
-            __tablename__ = 'states'
-                id = Column(Integer, primary_key=True)
-                    name = Column(String(128), nullable=False)
-                        cities = relationship("City", backref="state")
+    """Representation of a state"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state")
