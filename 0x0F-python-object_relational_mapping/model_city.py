f2538de243fd20749175b094a1b6314c4ce8843c
Akinloye Oluwapelumi <83060434+pelzmagic@users.noreply.github.com>
2022-11-09 10:23:09 -0800

    Update model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -9,8 +9,8 @@ from model_state import Base, State
 
 
 class City(Base):
-        """Representation of a city"""
-            __tablename__ = "cities"
-                id = Column(Integer, primary_key=True)
-                    name = Column(String(128), nullable=False)
-                        state_id = Column(Integer, ForeignKey('states.id'))
+    """Representation of a city"""
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
