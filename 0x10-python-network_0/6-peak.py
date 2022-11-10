4acb4cc1294fe1e46e442be0647427c3a8648637
Akinloye Oluwapelumi <83060434+pelzmagic@users.noreply.github.com>
2022-11-10 06:06:17 -0800

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,27 +7,27 @@ For corner elements, we need to consider only one neighbor.
 
 
 def find_peak(A):
-        """find pick element"""
-            if A == []:
-                        return None
+    """find pick element"""
+    if A == []:
+        return None
 
-                        def recursive(A, left=0, right=len(A) - 1):
-                                    """helper recursive function"""
+    def recursive(A, left=0, right=len(A) - 1):
+        """helper recursive function"""
 
-                                            mid = (left + right) // 2
+        mid = (left + right) // 2
 
-                                                    # check if the middle element is greater than its neighbors
-                                                            if ((mid == 0 or A[mid - 1] <= A[mid]) and
-                                                                                    (mid == len(A) - 1 or A[mid + 1] <= A[mid])):
-                                                                            return A[mid]
+        # check if the middle element is greater than its neighbors
+        if ((mid == 0 or A[mid - 1] <= A[mid]) and
+                (mid == len(A) - 1 or A[mid + 1] <= A[mid])):
+            return A[mid]
 
-                                                                                # If the left neighbor of `mid` is greater than the middle element,
-                                                                                        # find the peak recursively in the left sublist
-                                                                                                if mid - 1 >= 0 and A[mid - 1] > A[mid]:
-                                                                                                                return recursive(A, left, mid - 1)
+        # If the left neighbor of `mid` is greater than the middle element,
+        # find the peak recursively in the left sublist
+        if mid - 1 >= 0 and A[mid - 1] > A[mid]:
+            return recursive(A, left, mid - 1)
 
-                                                                                                                    # If the right neighbor of `mid` is greater than the middle element,
-                                                                                                                            # find the peak recursively in the right sublist
-                                                                                                                                    return recursive(A, mid + 1, right)
+        # If the right neighbor of `mid` is greater than the middle element,
+        # find the peak recursively in the right sublist
+        return recursive(A, mid + 1, right)
 
-                                                                                                                                    return recursive(A)
+    return recursive(A)
