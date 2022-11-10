d56f85b37df3502869317b9fc0ab090102dc1ce5
Akinloye Oluwapelumi <83060434+pelzmagic@users.noreply.github.com>
2022-11-10 06:46:24 -0800

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,6 +5,6 @@ import sys
 
 
 if __name__ == "__main__":
-        email = {'email': sys.argv[2]}
-            r = requests.post(sys.argv[1], data=email)
-                print(r.text)
+    email = {'email': sys.argv[2]}
+    r = requests.post(sys.argv[1], data=email)
+    print(r.text)
