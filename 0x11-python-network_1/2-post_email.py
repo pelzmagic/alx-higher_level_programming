601602e871cbba574350b573358c85c85ff80bd7
Akinloye Oluwapelumi <83060434+pelzmagic@users.noreply.github.com>
2022-11-10 06:24:49 -0800

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,10 +6,10 @@ import sys
 
 
 if __name__ == "__main__":
-        value = {'email': sys.argv[2]}
-            data = urllib.parse.urlencode(value)
-                data = data.encode('utf-8')
-                    req = urllib.request.Request(sys.argv[1], data)
-                        with urllib.request.urlopen(req) as response:
-                                    ce = response.read().decode(encoding='UTF-8')
-                                            print(ce)
+    value = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(value)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        ce = response.read().decode(encoding='UTF-8')
+        print(ce)
